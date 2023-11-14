e06a768621714990abb6b8a5c7e22fd88839025c
Patnaree Johnson <j.patnaree.t@gmail.com>
2023-11-14 17:33:58 -0500

    lab

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,29 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size 
+
+    @property
+    def size(self):
+        return self.size
+    
+    @size.setter
+    def size(self, size):
+        if isinstance(size, int):
+            self.size = size
+        else: 
+            print('size must be an integer')
+
+    @property
+    def brand(self):
+        return self.brand
+    
+    @brand.setter
+    def brand(self, brand):
+        self.brand = brand
+
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!")
